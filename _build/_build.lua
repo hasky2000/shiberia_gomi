2fe535b9b25ac5a37d5b794979f9396df97fd4e1
hasky2000 <124584756+hasky2000@users.noreply.github.com>
2024-08-23 11:41:19 +0900

    MOD追加
    航空レシプロ音mod追加＆導入方法書いた

---
+++ _build/_build.lua
@@ -0,0 +1,39 @@
+
+--- @diagnostic disable: undefined-global
+
+require("LifeBoatAPI.Tools.Build.Builder")
+
+-- replace newlines
+for k,v in pairs(arg) do
+    arg[k] = v:gsub("##LBNEWLINE##", "\n")
+end
+
+local luaDocsAddonPath  = LifeBoatAPI.Tools.Filepath:new(arg[1]);
+local luaDocsMCPath     = LifeBoatAPI.Tools.Filepath:new(arg[2]);
+local outputDir         = LifeBoatAPI.Tools.Filepath:new(arg[3]);
+local params            = {
+    boilerPlate             = arg[4],
+    reduceAllWhitespace     = arg[5] == "true",
+    reduceNewlines          = arg[6] == "true",
+    removeRedundancies      = arg[7] == "true",
+    shortenVariables        = arg[8] == "true",
+    shortenGlobals          = arg[9] == "true",
+    shortenNumbers          = arg[10]== "true",
+    forceNCBoilerplate      = arg[11]== "true",
+    forceBoilerplate        = arg[12]== "true",
+    shortenStringDuplicates = arg[13]== "true",
+    removeComments          = arg[14]== "true",
+    skipCombinedFileOutput  = arg[15]== "true"
+};
+local rootDirs          = {};
+
+for i=15, #arg do
+    table.insert(rootDirs, LifeBoatAPI.Tools.Filepath:new(arg[i]));
+end
+
+local _builder = LifeBoatAPI.Tools.Builder:new(rootDirs, outputDir, luaDocsMCPath, luaDocsAddonPath)
+
+if onLBBuildStarted then onLBBuildStarted(_builder, params, LifeBoatAPI.Tools.Filepath:new([[c:\Users\nao\Documents\GitHub\shiberia_gomi]])) end
+
+if onLBBuildComplete then onLBBuildComplete(_builder, params, LifeBoatAPI.Tools.Filepath:new([[c:\Users\nao\Documents\GitHub\shiberia_gomi]])) end
+--- @diagnostic enable: undefined-global
